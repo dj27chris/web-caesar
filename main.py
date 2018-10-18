d09b3d9bc4776783346eaf9b189129cfd3307a53
Debbie Christopher <debbie@tapsplace.com>
2018-10-17 22:07:18 -0500

    add function to use cypher

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import cgi
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -47,6 +48,15 @@ form = """
         </textarea>
         
 """
+
+@app.route("/", methods=['POST'])
+def encrypt
+    rot = request.form("rot")
+    text = request.form("text")
+
+    new_text = rotate_string(text, rot)
+
+    return '<h1>' + new_text + '</h1>'
 
 
 @app.route("/")
